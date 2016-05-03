dd79c48699aaaa59c9cfc854ec0e39a554397b4f
Ruslan Al-Fakikh <ruslan-al-fakikh@idexx.com>
2016-05-03 17:20:24 +0300

    after lein new

---
+++ test/clojure_tracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-tracker.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-tracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
